40512c8aee9155bbc0d466cd3883d413f341c59c
Olga Shopieva <shopieva94@gmail.com>
2024-01-30 13:46:09 +0300

    2 tasks done

--- src/main/java/com/example/leetcodetasks/LeetcodeTasksApplication.java
+++ src/main/java/com/example/leetcodetasks/LeetcodeTasksApplication.java
@@ -9,10 +9,8 @@ public class LeetcodeTasksApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeetcodeTasksApplication.class, args);
-        int[] array = {1, 0, 1, 1, 1, 0};
-        MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
-        maxConsecutiveOnes.findMaxConsecutiveOnes(array);
-        System.out.println(maxConsecutiveOnes.findMaxConsecutiveOnes(array));
+ 
+
     }
 
 }
