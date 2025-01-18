8b3051b92baa8274e985c884b49593860a965829
Olga <shopieva94@gmail.com>
2025-01-18 23:56:19 +0300

    RotateImage solved

---
+++ src/main/java/com/example/leetcodetasks/medium/Arrays/RotateImage.java
@@ -0,0 +1,59 @@
+package com.example.leetcodetasks.medium.Arrays;
+
+/**
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ * Example1:
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ */
+public class RotateImage {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Пример исходной матрицы
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Исходная матрица:");
+        printMatrix(matrix);
+
+        // Поворот матрицы
+        rotate(matrix);
+
+        System.out.println("Матрица после поворота на 90 градусов по часовой стрелке:");
+        printMatrix(matrix);
+    }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
