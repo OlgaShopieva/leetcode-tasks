a9bfdb320363f66f642b71cc284688ec2e3434cf
Olga <shopieva94@gmail.com>
2025-02-18 08:30:16 +0300

    FaultyKeyboard solved

---
+++ src/main/java/com/example/leetcodetasks/Main.java
@@ -0,0 +1,11 @@
+package com.example.leetcodetasks;
+
+import com.example.leetcodetasks.easy.String.FaultyKeyboard;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "poiinter";
+        FaultyKeyboard fk = new FaultyKeyboard();
+        System.out.println(fk.finalString(s));
+    }
+}
