ea6d658fe38594fb3ad0ade11b3d90ef605a7278
Olga <shopieva94@gmail.com>
2025-02-19 07:06:49 +0300

    CheckIfPangram solved

--- src/main/java/com/example/leetcodetasks/Main.java
+++ src/main/java/com/example/leetcodetasks/Main.java
@@ -1,11 +1,12 @@
 package com.example.leetcodetasks;
 
+import com.example.leetcodetasks.easy.String.CheckIfPangram;
 import com.example.leetcodetasks.easy.String.FaultyKeyboard;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "poiinter";
-        FaultyKeyboard fk = new FaultyKeyboard();
-        System.out.println(fk.finalString(s));
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        CheckIfPangram cp = new CheckIfPangram();
+        System.out.println(cp.checkIfPangram(sentence));
     }
 }
