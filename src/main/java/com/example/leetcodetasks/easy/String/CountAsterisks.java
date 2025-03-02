c0309bfcd918680c9b29c6a15a16ae04e1ae0862
Olga <shopieva94@gmail.com>
2025-03-02 23:16:41 +0300

    CountAsterisks solved

---
+++ src/main/java/com/example/leetcodetasks/easy/String/CountAsterisks.java
@@ -0,0 +1,29 @@
+package com.example.leetcodetasks.easy.String;
+
+/**
+ * You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair.
+ * In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+ * Return the number of '*' in s, excluding the '*' between each pair of '|'.
+ * Note that each '|' will belong to exactly one pair.
+ * Example 1:
+ * Input: s = "l|*e*et|c**o|*de|"
+ * Output: 2
+ * Explanation: The considered characters are underlined: "l*|*e*et|c**o|*de|".
+ * The characters between the first and second '|' are excluded from the answer.
+ * Also, the characters between the third and fourth '|' are excluded from the answer.
+ * There are 2 asterisks considered. Therefore, we return 2.
+ */
+public class CountAsterisks {
+
+    public int countAsterisks(String s) {
+        int countAster = 0;
+        int countBars = 0;
+        for(char ch: s.toCharArray()) {
+            if(ch == '*' && countBars % 2 == 0) {
+                countAster++;
+            }
+            if(ch == '|') countBars++;
+        }
+        return countAster;
+    }
+}
