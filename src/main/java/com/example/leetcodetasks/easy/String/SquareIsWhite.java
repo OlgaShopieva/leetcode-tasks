3401238a7aaeb071d5c933594392a1d9770d7c09
Olga <shopieva94@gmail.com>
2025-03-15 20:38:03 +0300

    SquareIsWhite solved

---
+++ src/main/java/com/example/leetcodetasks/easy/String/SquareIsWhite.java
@@ -0,0 +1,27 @@
+package com.example.leetcodetasks.easy.String;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * You are given coordinates, a string that represents the coordinates of a square of the chessboard.
+ * Below is a chessboard for your reference.
+ * Return true if the square is white, and false if the square is black.
+ * The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first,
+ * and the number second.
+ * Example 1:
+ * Input: coordinates = "a1"
+ * Output: false
+ * Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+ */
+public class SquareIsWhite {
+
+    public static void main(String[] args) {
+        String s = "a2";
+        System.out.println(squareIsWhite(s));
+    }
+
+    public static boolean squareIsWhite(String coordinates) {
+        return (coordinates.charAt(0) - 'a' + 1) % 2 != (coordinates.charAt(1) - '0') % 2;
+    }
+}
