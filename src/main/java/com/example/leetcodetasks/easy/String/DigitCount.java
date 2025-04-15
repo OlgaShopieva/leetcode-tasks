1b516cdfe83a8ea86a5935a0d68ac1217460d594
Olga <shopieva94@gmail.com>
2025-04-16 00:13:07 +0300

    DigitCount solved

---
+++ src/main/java/com/example/leetcodetasks/easy/String/DigitCount.java
@@ -0,0 +1,31 @@
+package com.example.leetcodetasks.easy.String;
+
+public class DigitCount {
+
+    public static void main(String[] args) {
+        String num = "1210";
+        System.out.println(digitCount(num));
+    }
+
+    public static boolean digitCount(String num) {
+        int n = num.length();
+
+        int[] frequency = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            char c = num.charAt(i);
+            if (Character.isDigit(c)) {
+                int digit = c - '0';
+                frequency[digit]++;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            int digit = num.charAt(i) - '0';
+            if(frequency[i] != digit) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
